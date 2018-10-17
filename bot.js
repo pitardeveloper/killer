3c34f9ca267fbaab2e9636191085189330e57387
aboali117 <44244818+aboali117@users.noreply.github.com>
2018-10-17 23:09:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,15 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-
+var prefix = "+";
 client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Death Shop`,"http://twitch.tv/Death Shop")
+client.user.setStatus("dnd")
 });
 
 client.on('message', msg => {
